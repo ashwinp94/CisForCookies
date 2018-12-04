eee5a66eaacc477e803baaf28e955c863b6d7482
Ashwin Prakash <ashp94@gmail.com>
2018-12-04 10:45:09 -0600

    I think I fixed it

---
+++ cookies.js
@@ -0,0 +1,18 @@
+// Original Code that needed to be corrected
+
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
+
+// for (let x = 1; y < cookies.length; x++) {
+//     const currentCookie = cookies[y]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+const currentCookie = cookies[y];
+
+for (let x = 1; x < cookies.length; x++) {
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
+
